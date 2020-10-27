bd844290495e2369525955aede698d5036796c5e
namjin <domino8788@naver.com>
2020-10-28 00:52:20 +0900

    (#51) feat: label 모델 구현

---
+++ backend/models/label.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'label',
+    {
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      color: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+    },
+    {
+      underscored: true,
+      tableName: 'label',
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+};
