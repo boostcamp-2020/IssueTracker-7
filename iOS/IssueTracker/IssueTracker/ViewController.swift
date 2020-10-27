260c20d30458e4560fdfe1d20afcb65147f90a81
tksrl0379 <tksrl0379@naver.com>
2020-10-27 14:28:16 +0900

    Initial commit

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by a1111 on 2020/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
