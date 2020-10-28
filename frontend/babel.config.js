83e70e5cfb430366bad339a18d6d91377c4dc449
ahnsohee <sohee@ahnsoheeui-MacBookPro.local>
2020-10-28 13:04:12 +0900

    (#33) feat: 바벨 재설정

---
+++ frontend/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function(api) {
+    api.cache(true);
+  
+    const presets = ["@babel/preset-env", "@babel/preset-react"];
+  
+    const plugins = [];
+  
+    return {
+      presets,
+      plugins
+    };
+  };
