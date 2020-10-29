c26cfaab924c1168481eec2c96d3e16cf06b1e9a
tksrl0379 <tksrl0379@naver.com>
2020-10-29 15:40:09 +0900

    (#44) feat: cell 선택 시 이전 controller 에게 필터 정보 전달
    What
    1.  FilteringTableViewController(static cell Tableview) 에서 cell 클릭 시 FilteringController 로 필터 조건 정보를 전달합니다

--- iOS/IssueTracker/IssueTracker/Controller/FilteringController.swift
+++ iOS/IssueTracker/IssueTracker/Controller/FilteringController.swift
@@ -13,12 +13,27 @@ class FilteringController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "FilteringTableView" {
+            guard let childVc = segue.destination as? FilteringTableViewController else { return }
+            childVc.delegate = self
+        }
+    }
+    
     @IBAction func doneButtonTapped(_ sender: Any) {
         // SignInController 로 세부 조건 전달
         dismiss(animated: true)
     }
     
     @IBAction func closeButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
+
+extension FilteringController: SendFilterConditionDelegate {
+    // 선택한 조건 수신 시 바로 화면 dismiss
+    func send(condition: Issuecondition) {
+        print(condition)
         dismiss(animated: true)
     }
 }
--- iOS/IssueTracker/IssueTracker/Controller/FilteringTableViewController.swift
+++ iOS/IssueTracker/IssueTracker/Controller/FilteringTableViewController.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
+enum Issuecondition {
+    case openIssue
+    case myIssue
+    case assignedIssue
+    case commentedIssue
+    case closedIssued
+}
+
+protocol SendFilterConditionDelegate: AnyObject {
+    func send(condition: Issuecondition)
+}
+
 class FilteringTableViewController: UITableViewController {
+    
+    weak var delegate: SendFilterConditionDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +29,38 @@ class FilteringTableViewController: UITableViewController {
     }
     
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        switch indexPath.section {
+        /*
+         1. 다음 중에 조건을 고르세요
+         열린 이슈들
+         내가 작성한 이슈들
+         나한테 할당된 이슈들
+         내가 댓글을 남긴 이슈들
+         닫힌 이슈들
+         */
+        case 0:
+            switch indexPath.row {
+            case 0:
+                delegate?.send(condition: .openIssue)
+            case 1:
+                delegate?.send(condition: .myIssue)
+            case 2:
+                delegate?.send(condition: .assignedIssue)
+            case 3:
+                delegate?.send(condition: .commentedIssue)
+            case 4:
+                delegate?.send(condition: .closedIssued)
+            default:
+                break
+            }
+        default:
+            break
+        }
+        
+        
+    }
     
 }
