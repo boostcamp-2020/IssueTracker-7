fe3d606d3e9138494e57833afb028f0081dbb2ae
Eric Jang <940709@gmail.com>
2020-10-29 14:13:07 +0900

    (#36) feat: IssueListViewController 파일 생성

---
+++ iOS/IssueTracker/IssueTracker/Controller/IssueListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssueListViewController.swift
+//  IssueTracker
+//
+//  Created by Imho Jang on 2020/10/29.
+//
+
+import UIKit
+
+class IssueListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
