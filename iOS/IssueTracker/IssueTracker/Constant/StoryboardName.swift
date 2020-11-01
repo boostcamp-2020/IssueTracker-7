eb1c19916bb1a160020fd2a59acff0a85c178c4d
tksrl0379 <tksrl0379@naver.com>
2020-11-01 21:13:24 +0900

    (#81) feat: 스토리보드 관련 Constant 파일 추가
    Why
    1. 스토리보드를 모듈화하다보니 일일이 문자열로 써주는 것보다는 상수로 표현하는 것이 더 혼동의 여지를 줄일 수 있다고 생각되어 추가했습니다.

---
+++ iOS/IssueTracker/IssueTracker/Constant/StoryboardName.swift
@@ -0,0 +1,13 @@
+//
+//  StoryboardName.swift
+//  IssueTracker
+//
+//  Created by a1111 on 2020/11/01.
+//
+
+import Foundation
+
+enum StoryboardName {
+    static let issueList = "IssueList" // 스토리보드(.storyboard) 파일 이름
+    static let issueListInitViewController = "IssueListTabBarController" // 해당 스토리보드 파일의 init viewcontroller 이름
+}
