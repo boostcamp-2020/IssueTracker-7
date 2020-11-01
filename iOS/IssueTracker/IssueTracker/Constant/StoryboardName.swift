890e3a151781a1d38e7078b4d15801306b34330e
tksrl0379 <tksrl0379@naver.com>
2020-11-01 22:42:43 +0900

    (#81) refactor: IssueList 관련 스토리보드 Constant 변경
    Why
    1. 뜻을 좀 더 명확히 전달하기 위해 변경했습니다

--- iOS/IssueTracker/IssueTracker/Constant/StoryboardName.swift
+++ iOS/IssueTracker/IssueTracker/Constant/StoryboardName.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
+/*
+[형식]
+ 1. 스토리보드(.storyboard) 파일 이름
+ 2. 해당 스토리보드 파일의 init viewcontroller 의 storyboard id
+ 
+ */
+
 enum StoryboardName {
-    static let issueList = "IssueList" // 스토리보드(.storyboard) 파일 이름
-    static let issueListInitViewController = "IssueListInitController" // 해당 스토리보드 파일의 init viewcontroller 이름
+    static let issueList = "IssueList"
+    static let issueListStoryboardInitialViewController = "IssueListStoryboardInitialViewController"
 }
