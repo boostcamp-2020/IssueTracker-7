5dbad4d827f2e2ce9f48e8aa1b45916df324a437
tksrl0379 <tksrl0379@naver.com>
2020-11-01 21:39:45 +0900

    (#81) refactor: controller storyboard id 변경
    Why
    1. storyboard id 만 보고 무슨 뜻인지 알 수 있도록 좀 더 구체적으로 변경했습니다

--- iOS/IssueTracker/IssueTracker/Constant/StoryboardName.swift
+++ iOS/IssueTracker/IssueTracker/Constant/StoryboardName.swift
@@ -9,5 +9,5 @@ import Foundation
 
 enum StoryboardName {
     static let issueList = "IssueList" // 스토리보드(.storyboard) 파일 이름
-    static let issueListInitViewController = "IssueListTabBarController" // 해당 스토리보드 파일의 init viewcontroller 이름
+    static let issueListInitViewController = "IssueListInitController" // 해당 스토리보드 파일의 init viewcontroller 이름
 }
