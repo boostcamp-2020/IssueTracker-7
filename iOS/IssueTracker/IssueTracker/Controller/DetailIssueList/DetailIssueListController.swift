416aa6dd202a56443829c963fb84f83237cccc26
tksrl0379 <tksrl0379@naver.com>
2020-11-03 18:17:01 +0900

    (#45) feat: 드래그 할 수 있는 view 생성
    What
    1. 다른 프로젝트에서 테스트하고 올리는 바람에 스토리보드 및 코드를 한꺼번에 커밋하게 됐습니다

--- iOS/IssueTracker/IssueTracker/Controller/DetailIssueList/DetailIssueListController.swift
+++ iOS/IssueTracker/IssueTracker/Controller/DetailIssueList/DetailIssueListController.swift
@@ -19,7 +19,27 @@ class DetailIssueListController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
+        let vc = storyboard?.instantiateViewController(identifier: "ReactionViewController") as! ReactionViewController
         
+        vc.modalPresentationStyle = .fullScreen
+        
+        // 딜레이를 넣어야 누른 버튼이 희미해지는 현상 해결 가능
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            // tabbar.view 로 해야 탭바까지 스냅샷
+            vc.backingImage = self.view.asImage()
+            
+            self.present(vc, animated: false) // false 로 해야 default 애니메이션을 안 쓸 수 있음
+        }
     }
     
+}
+
+extension UIView  {
+    // render the view within the view's bounds, then capture it as image
+  func asImage() -> UIImage {
+    let renderer = UIGraphicsImageRenderer(bounds: bounds)
+    return renderer.image(actions: { rendererContext in
+        layer.render(in: rendererContext.cgContext)
+    })
+  }
 }
