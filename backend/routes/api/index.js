7abfa158bbaf332d912a429ffccf082dcc4468ae
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 12:03:57 +0900

    (#5) feat: user 라우터 추가

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const auth = require('./auth');
+const user = require('./user')
 const { verifyToken } = require('../../middlewares/token');
 
 router.use('/auth', auth);
 router.use(verifyToken('jwt'));
+router.use('/user', user)
 
 module.exports = router;
