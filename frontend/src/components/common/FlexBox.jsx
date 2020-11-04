3474cd63e2a54c052c4cb255b62fb813ddb5a9bc
namjin <domino8788@naver.com>
2020-11-05 00:28:45 +0900

    (#115) feat: 세미콜론 누락 사항 및 오타 수정

--- frontend/src/components/common/FlexBox.jsx
+++ frontend/src/components/common/FlexBox.jsx
@@ -4,6 +4,5 @@ import styled from "styled-components";
 const FlexBox = styled.div`
 display:flex;
 flex-direction:${(props)=>props.direction || "row"};
-justify-contents:
-`
+`;
 export default FlexBox;
