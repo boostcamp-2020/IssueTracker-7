42352de01362c25af8d6476b67ea31bc834647f9
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 14:56:56 +0900

    (#104) feat: async 제거, then/catch 사용

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,8 +1,8 @@
 const { User } = require('../models');
 
-exports.get = async (req, res) => {
+exports.get = (req, res) => {
     const users = await User.findAll({
         attributes: ['id', 'user_id', 'photo_url', 'type']
-    });
-    res.json(users);
+    }).then(users => res.status(200).json(users))
+    .catch(err => res.status(401).send('유효하지 않은 요청입니다.'))
 };
