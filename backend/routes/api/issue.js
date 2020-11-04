1f79c2f44dd30749fcdceab1052481b0843e4a66
namjin <domino8788@naver.com>
2020-11-04 16:07:34 +0900

    (#4) feat: issue의 상태를 변경하는 라우팅 추가

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const issueController = require('../../controllers/issue');
 
 router.get('/', issueController.get);
+router.put('/:issue_id', issueController.update);
 
 module.exports = router;
