8e55bae2f4fa10aa4a20d18cc3ca818a2f9c9c6f
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 12:05:14 +0900

    (#5) style: 세미콜론 추가

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const auth = require('./auth');
-const user = require('./user')
+const user = require('./user');
 const { verifyToken } = require('../../middlewares/token');
 
 router.use('/auth', auth);
 router.use(verifyToken('jwt'));
-router.use('/user', user)
+router.use('/user', user);
 
 module.exports = router;
