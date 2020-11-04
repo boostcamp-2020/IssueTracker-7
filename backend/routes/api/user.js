6620715fbfbd0ed550e9e6d022fca89bd8db189a
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 12:04:44 +0900

    (#5) style: 세미콜론 추가

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../../controller/user')
+const userController = require('../../controller/user');
 
-router.get('/', userController.get)
+router.get('/', userController.get);
 
 module.exports = router; 
