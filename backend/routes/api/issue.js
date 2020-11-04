465241d102343f52349f157e9cce086456898a2a
namjin <domino8788@naver.com>
2020-11-04 14:20:03 +0900

    (#3) style: 불필요한 주석 제거

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const issueController = require('../../controllers/issue');
 
-/* GET home page. */
 router.get('/', issueController.get);
 
 module.exports = router;
