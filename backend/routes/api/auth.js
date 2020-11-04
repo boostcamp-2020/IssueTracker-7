331115335ac45405ed8c200d9210bfe6a85fa085
namjin <domino8788@naver.com>
2020-11-04 12:13:38 +0900

    (#2) feat: github oauth에서 ios와 web의 요청 경로를 분리

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 const { createToken, verifyToken } = require('../../middlewares/token');
 
-router.get('/github', verifyToken('github'));
+router.get(
+  '/github/web',
+  (req, res, next) => {
+    res.cookie('media_type', 'web', { signed: true, httpOnly: true });
+    next();
+  },
+  verifyToken('github')
+);
+router.get(
+  '/github/ios',
+  (req, res, next) => {
+    res.cookie('media_type', 'ios', { signed: true, httpOnly: true });
+    next();
+  },
+  verifyToken('github')
+);
 
 router.get(
   '/github/callback',
@@ -14,7 +29,16 @@ router.get(
   (req, res) => {
     res.cookie('accessToken', res.locals.accessToken, { signed: true, httpOnly: true });
     res.cookie('refreshToken', res.locals.refreshToken, { signed: true, httpOnly: true });
-    return res.redirect('/');
+    let redirectUrl = null;
+    switch (req.signedCookies.media_type) {
+      case 'web':
+        redirectUrl = '/';
+        break;
+      case 'ios':
+        redirectUrl = `IssueTracker://login#accessToken=${res.locals.accessToken}refreshToken=${res.locals.refreshToken}`;
+        break;
+    }
+    return res.redirect(redirectUrl);
   }
 );
 
