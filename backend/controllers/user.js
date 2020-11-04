f6511916ca97c3291928f5eaf92969fb7fa44b5e
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 16:05:03 +0900

    (#104) chore: 세미콜론 추가

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,5 +4,5 @@ exports.get = (req, res) => {
     User.findAll({
         attributes: ['id', 'user_id', 'photo_url', 'type']
     }).then(users => res.status(200).json(users))
-    .catch(err => res.status(401).send('유효하지 않은 요청입니다.'))
+        .catch(err => res.status(401).send('유효하지 않은 요청입니다.'));
 };
