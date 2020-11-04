593011a85345159b98d65f1c233e5d34c4089561
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 15:12:10 +0900

    (#104) feat: 중복된 변수명 사용한 부분 수정

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,7 +1,7 @@
 const { User } = require('../models');
 
 exports.get = (req, res) => {
-    const users = await User.findAll({
+    User.findAll({
         attributes: ['id', 'user_id', 'photo_url', 'type']
     }).then(users => res.status(200).json(users))
     .catch(err => res.status(401).send('유효하지 않은 요청입니다.'))
