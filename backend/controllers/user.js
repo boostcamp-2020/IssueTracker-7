78799354b4a986ed4bb726e82b61926c252c06aa
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 15:17:37 +0900

    (#104) chore: user 컨트롤러 controllers 디렉토리로 이동

---
+++ backend/controllers/user.js
@@ -0,0 +1,8 @@
+const { User } = require('../models');
+
+exports.get = (req, res) => {
+    User.findAll({
+        attributes: ['id', 'user_id', 'photo_url', 'type']
+    }).then(users => res.status(200).json(users))
+    .catch(err => res.status(401).send('유효하지 않은 요청입니다.'))
+};
