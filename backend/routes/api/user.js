a1c165f63da71ac8e9bb8773b610a20480b8e53e
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 12:00:51 +0900

    (#5) feat: 전체 사용자 데이터를 반환하는 API 등록

---
+++ backend/routes/api/user.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../../controller/user')
+
+router.get('/', userController.get)
+
+module.exports = router; 
