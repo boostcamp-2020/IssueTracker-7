5cae261c7e227d03d8d05467f4b40dfdcd566231
tksrl0379 <tksrl0379@naver.com>
2020-11-04 12:06:52 +0900

    (#94) refactor: 사용성 관련 수정
    What
    1. 뒷 배경을 누르면 카드뷰가 최소화 되도록 변경했습니다
    2. blur 를 없애고 회색바탕만 나오도록 변경했습니다

--- iOS/IssueTracker/IssueTracker/Controller/DetailIssueList/DetailIssueListController.swift
+++ iOS/IssueTracker/IssueTracker/Controller/DetailIssueList/DetailIssueListController.swift
@@ -7,14 +7,25 @@
 
 import UIKit
 
-class DetailIssueListController: UIViewController {
+final class DetailIssueListController: UIViewController {
+    
+    // MARK: - Enum
     
     enum CardState {
         case collapsed
         case expanded
     }
     
-    @IBOutlet weak var dimmerView: UIView!
+    
+    // MARK: - Property
+    
+    var cardView: CardView!
+    lazy var dimmerView: UIView = {
+        let dimmerView = UIView()
+        dimmerView.backgroundColor = UIColor.gray
+        dimmerView.frame = self.view.bounds
+        return dimmerView
+    }()
     
     lazy var cardStartY: CGFloat = view.bounds.height * 0.1
     lazy var cardEndY: CGFloat = view.bounds.height * 0.85
@@ -22,12 +33,12 @@ class DetailIssueListController: UIViewController {
     lazy var cardLatestY : CGFloat = cardEndY // 제스쳐 start 시 갱신되는 가장 최신의 Y 좌표
     var cardCurrentState: CardState = .collapsed
     
-    var cardViewController:CardViewController!
-    var visualEffectView:UIVisualEffectView!
     
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         dimmerView.isUserInteractionEnabled = false
         dimmerView.alpha = 0
@@ -42,30 +53,44 @@ class DetailIssueListController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        visualEffectView.removeFromSuperview()
-        cardViewController.view.removeFromSuperview()
+        dimmerView.removeFromSuperview()
+        cardView.removeFromSuperview()
     }
     
+    
+    // MARK: - Method
+    
     func setupCard() {
+        tabBarController?.view.addSubview(dimmerView)
         
-        visualEffectView = UIVisualEffectView()
-        visualEffectView.frame = tabBarController!.view.bounds
-        visualEffectView.isUserInteractionEnabled = false
-        tabBarController?.view.addSubview(visualEffectView)
-        
-        cardViewController = CardViewController(nibName: "CardViewController", bundle: nil)
-        tabBarController?.view.addSubview(cardViewController.view)
-        self.cardViewController.view.frame = CGRect(x: 0, y: view.bounds.height, width: self.view.bounds.width, height: self.view.frame.height - cardStartY)
+        cardView = CardView()
+        tabBarController?.view.addSubview(cardView)
 
-        UIViewPropertyAnimator(duration: 0.1, curve: .easeIn) {
-            self.cardViewController.view.frame.origin.y = self.cardEndY
-        }.startAnimation()
-        cardViewController.view.clipsToBounds = true
-        cardViewController.view.layer.cornerRadius = 15.0
-        cardViewController.view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        cardView.frame = CGRect(
+            x: 0,
+            y: view.bounds.height,
+            width: self.view.bounds.width,
+            height: self.view.frame.height - cardStartY
+        )
         
+        UIViewPropertyAnimator(duration: 0.1, curve: .easeIn) {
+            self.cardView.frame.origin.y = self.cardEndY
+        }.startAnimation()
+
+        cardView.clipsToBounds = true
+        cardView.layer.cornerRadius = 15.0
+        cardView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+
         let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleCardPan(recognizer:)))
-        cardViewController.view.addGestureRecognizer(panGestureRecognizer)
+        cardView.addGestureRecognizer(panGestureRecognizer)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dimmerViewTapped))
+        dimmerView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc
+    func dimmerViewTapped() {
+        animateTransitionIfNeeded(state: .collapsed, duration: 0.1) // 최소화
     }
     
     @objc
@@ -73,22 +98,22 @@ class DetailIssueListController: UIViewController {
 
         switch recognizer.state {
         case .began:
-            cardLatestY = cardViewController.view.frame.origin.y
+            cardLatestY = cardView.frame.origin.y
         case .changed:
-            let translation = recognizer.translation(in: cardViewController.view)
+            let translation = recognizer.translation(in: cardView)
             let expectedY = cardLatestY + translation.y
             
             if (cardStartY...cardEndY) ~= expectedY {
-                cardViewController.view.frame.origin.y = cardLatestY + translation.y
+                cardView.frame.origin.y = cardLatestY + translation.y
             }
         case .ended:
             switch cardCurrentState {
             case .collapsed:
-                if cardViewController.view.frame.origin.y < cardLatestY {
+                if cardView.frame.origin.y < cardLatestY {
                     animateTransitionIfNeeded(state: .expanded, duration: 0.1) // 확장
                 }
             case .expanded:
-                if cardViewController.view.frame.origin.y > cardLatestY {
+                if cardView.frame.origin.y > cardLatestY {
                     animateTransitionIfNeeded(state: .collapsed, duration: 0.1) // 최소화
                 }
             }
@@ -97,32 +122,30 @@ class DetailIssueListController: UIViewController {
         }
     }
     
-    // Animate transistion function
     func animateTransitionIfNeeded (state: CardState, duration: TimeInterval) {
         
         // TODO: 추후 UIViewPropertyAnimator fractionComplete 활용해서 더 interactive 하게 만들어보기
         let frameAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn) {
             switch state {
             case .expanded:
-                self.cardViewController.view.frame.origin.y = self.cardStartY
+                self.cardView.frame.origin.y = self.cardStartY
                 self.dimmerView.alpha = 0.7
-                self.visualEffectView.effect = UIBlurEffect(style: .dark)
-                self.visualEffectView.alpha = 0.65
+                self.dimmerView.isUserInteractionEnabled = true
                 
                 self.cardCurrentState = .expanded
             case .collapsed:
-                self.cardViewController.view.frame.origin.y = self.cardEndY
+                self.cardView.frame.origin.y = self.cardEndY
                 self.dimmerView.alpha = 0
-                self.visualEffectView.effect = nil
-                self.visualEffectView.alpha = 1
-                
+                self.dimmerView.isUserInteractionEnabled = false
+        
                 self.cardCurrentState = .collapsed
             }
         }
         
-        cardLatestY = cardViewController.view.frame.origin.y
+        cardLatestY = cardView.frame.origin.y
     
         frameAnimator.startAnimation()
     }
+    
 }
 
