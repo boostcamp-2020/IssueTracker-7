2f97a878190540d1a0ba775e3d3f4c414ecd507d
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 13:18:21 +0900

    (#104) feat: user controller에 전체 사용자 정보를 반환하는 기능 구현

---
+++ backend/controller/user.js
@@ -0,0 +1,8 @@
+const { User } = require('../models');
+
+exports.get = async (req, res) => {
+    const users = await User.findAll({
+        attributes: ['id', 'user_id', 'photo_url', 'type']
+    });
+    res.json(users);
+};
