b5b4e4fd856af70232d1afc8377347f959788d5b
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 15:15:45 +0900

    (#104) chore: user 컨트롤러 파일 controllers 디렉토리로 이동

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../../controller/user');
+const userController = require('../../controllers/user');
 
 router.get('/', userController.get);
 
