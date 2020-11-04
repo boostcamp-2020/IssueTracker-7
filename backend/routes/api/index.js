2927f6ab479d21854afb482409e0d56b70d17e13
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-04 16:03:22 +0900

    (#7) feat: 마일스톤 라우터 등록

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const auth = require('./auth');
 const user = require('./user');
 const issue = require('./issue');
-const label = require('./label')
+const label = require('./label');
+const milestone = require('./milestone');
 const { verifyToken } = require('../../middlewares/token');
 
 router.use('/auth', auth);
@@ -11,5 +12,6 @@ router.use(verifyToken('jwt'));
 router.use('/user', user);
 router.use('/issue', issue);
 router.use('/label', label);
+router.use('/milestone', milestone);
 
 module.exports = router;
