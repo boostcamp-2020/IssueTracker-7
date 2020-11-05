16e4a52e08ffcc4b2809f242ad9f93952112e52b
Eric Jang <940709@gmail.com>
2020-11-05 18:51:44 +0900

    (#43) feat: 마일스톤 생성화면 추가

--- iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneViewController.swift
+++ iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneViewController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class ManageMilestoneViewController: UIViewController {
+final class ManageMilestoneViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        
+        configureLayout()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Layout
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension ManageMilestoneViewController {
+    
+    private func configureLayout() {
+        
+        let collectionViewFlowLayout = UICollectionViewFlowLayout()
+        collectionViewFlowLayout.itemSize = CGSize(width: view.bounds.size.width, height: 77)
+        collectionViewFlowLayout.minimumLineSpacing = 1
+        collectionViewFlowLayout.sectionInset = UIEdgeInsets(top: 2, left: 0, bottom: 0, right: 0)
+        collectionView.collectionViewLayout = collectionViewFlowLayout
     }
-    */
+}
 
+// MARK: - DataSource
+
+extension ManageMilestoneViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MilestoneCell.reuseIdentifier, for: indexPath)
+        // TODO: 각 cell을 터치했을 때 편집화면으로 이동해야한다.
+        return cell
+    }
+}
+
+// MARK: - MilestoneCell
+
+final class MilestoneCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = String(describing: MilestoneCell.self)
+    @IBOutlet var name: UIButton!
+    @IBOutlet var dueDate: UILabel!
+    @IBOutlet var percentComplete: UILabel!
+    @IBOutlet var info: UILabel!
+    @IBOutlet var openIssueCount: UILabel!
+    @IBOutlet var closedIssueCount: UILabel!
 }
