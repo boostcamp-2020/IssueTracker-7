e9e149674dcc0d6a50b5983e5174c27ef9b95f1e
Eric Jang <940709@gmail.com>
2020-11-05 17:26:09 +0900

    (#43) feat: 마일스톤 생성 모달 커스텀 뷰 추가

--- iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneModalView.swift
+++ iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneModalView.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
+@IBDesignable
 class ManageMilestoneModalView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
     }
-    */
-
 }
