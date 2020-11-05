fe097e3701ebfec9d1a93a133f62b7968167ded9
Eric Jang <940709@gmail.com>
2020-11-05 17:24:17 +0900

    (#43) feat: 마일스톤 관리화면 뷰 구현을 위한 파일 추가

---
+++ iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ManageMilestoneModalView.swift
+//  IssueTracker
+//
+//  Created by Imho Jang on 2020/11/05.
+//
+
+import UIKit
+
+class ManageMilestoneModalView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS/IssueTracker/IssueTracker/Controller/ManageMilestone/ManageMilestoneModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManageMilestoneModalViewController.swift
+//  IssueTracker
+//
+//  Created by Imho Jang on 2020/11/05.
+//
+
+import UIKit
+
+class ManageMilestoneModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
