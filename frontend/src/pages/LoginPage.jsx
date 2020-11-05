7a6e8c8ccb45b5dc06bf6156a0c5938519cb4b9a
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-05 12:52:58 +0900

    (#12) feat: 이미지 버튼 가져오는 방식 수정

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import GithubButton from '../components/login/GithubButton';
-import{ FontAwesomeIcon }from"@fortawesome/react-fontawesome";
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-free/js/all';
 
 const LoginPage = () => {
     const handleGitHubLogin = () => {
         location.href = '/api/auth/github/web';
     };
     return (
-        <GithubButton onClick={handleGitHubLogin}>Sign with GitHub <FontAwesomeIcon icon={faGithub} /></GithubButton>
+        <GithubButton onClick={handleGitHubLogin}>Sign with GitHub <i className="fab fa-github" /></GithubButton>
     );
 };
 
