1261f853e61e8fef67343a9fe6b909e5a1a860eb
namjin <domino8788@naver.com>
2020-11-06 11:07:05 +0900

    (#159) feat: 웹페이지를 반환하는 라우팅 구현

--- backend/app.js
+++ backend/app.js
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 passportConfig(passport);
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
+app.use((req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
