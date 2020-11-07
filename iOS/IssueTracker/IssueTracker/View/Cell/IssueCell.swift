45d98716098451f0bdf2bf5eca89a70f7b7eb5a0
tksrl0379 <tksrl0379@naver.com>
2020-11-07 15:09:10 +0900

    (#38) feat: 기존 이슈 cell 의 컴포넌트를 가지는 UIView 생성
    What
    1. cell 이 가지고 있던 title, label 등의 IBOutlet 을 모두 새로운 UIView 로 넣었습니다
    2. 그리고 이 UIView 는 swipe 기능을 위해 사용되는 StackView 에 들어갈 예정입니다

--- iOS/IssueTracker/IssueTracker/View/Cell/IssueCell.swift
+++ iOS/IssueTracker/IssueTracker/View/Cell/IssueCell.swift
@@ -12,10 +12,6 @@ final class IssueCell: UICollectionViewCell {
     // MARK: - Property
     
     static let reuseIdentifier = String(describing: IssueCell.self)
-    @IBOutlet var title: UILabel!
-    @IBOutlet var content: UILabel!
-    @IBOutlet var milestone: UIButton!
-    @IBOutlet weak var labelStackView: UIStackView!
     
     
     // MARK: - Initializer
@@ -35,45 +31,17 @@ final class IssueCell: UICollectionViewCell {
     // MARK: - Method
     
     override func prepareForReuse() {
-        labelStackView.arrangedSubviews.forEach {
-            $0.removeFromSuperview()
-        }
+        visibleView.initLabels()
+    }
     }
     
-    func setUpView() {
+    private func setUpView() {
         layer.cornerRadius = 10
         layer.masksToBounds = true
     }
-    
-    func hexStringToUIColor (hex: String) -> UIColor {
-        var rgbValue: UInt64 = 0
-        let droppedString = hex.dropFirst()
 
-        Scanner(string: String(droppedString)).scanHexInt64(&rgbValue)
-
-        return UIColor(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
-    }
-    
     func configure(issueData: IssueData) {
-        title.text = issueData.title
-        // cell.content.text = // TODO: API 쪽에서 아직 구현이 안되서 추후 수정
-        milestone.setTitle(issueData.milestone.title, for: .normal)
-        
-        issueData.labels?.forEach {
-            let btn = UIButton()
-            btn.setTitle(" \($0.name) ", for: .normal)
-            btn.backgroundColor = hexStringToUIColor(hex: $0.color)
-            btn.setTitleColor(UIColor.black, for: .normal)
-            btn.titleLabel?.font = .systemFont(ofSize: 15)
-            btn.cornerRadius = 5
-            
-            labelStackView.addArrangedSubview(btn)
-        }
+        visibleView.configure(issueData: issueData)
     }
 }
 
