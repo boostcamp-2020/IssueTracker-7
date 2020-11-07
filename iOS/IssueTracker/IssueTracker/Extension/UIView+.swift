2f20730685eeb62aa802f4591293b3db77c314b7
tksrl0379 <tksrl0379@naver.com>
2020-11-07 15:05:23 +0900

    (#38) feat: 상위뷰와 제약 조건 지정하는 UIView extension 추가
    What
    1. 상위뷰의 상하좌우에 대해 같은 제약조건을 가지도록 하는 메서드입니다

--- iOS/IssueTracker/IssueTracker/Extension/UIView+.swift
+++ iOS/IssueTracker/IssueTracker/Extension/UIView+.swift
@@ -15,4 +15,14 @@ extension UIView {
         self.layer.shadowRadius = radius
         self.layer.shadowOpacity = opacity
     }
+    
+    func pinEdgesToSuperView() {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leftAnchor.constraint(equalTo: superView.leftAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        rightAnchor.constraint(equalTo: superView.rightAnchor).isActive = true
+    }
 }
+
