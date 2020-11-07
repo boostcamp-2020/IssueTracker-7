e745257c84a69108cd24e22d06d12941dd7c7c79
tksrl0379 <tksrl0379@naver.com>
2020-11-07 15:10:25 +0900

    (#38) feat: Swipe 기능 구현을 위한 컴포넌트들 추가
    What
    1. 기본적으로 cell 위에 ScrollView -> StackView -> UIView 3개(내용, 닫기(close), delete(삭제)) 버튼이 깔리게 됩니다

--- iOS/IssueTracker/IssueTracker/View/Cell/IssueCell.swift
+++ iOS/IssueTracker/IssueTracker/View/Cell/IssueCell.swift
@@ -13,6 +13,25 @@ final class IssueCell: UICollectionViewCell {
     
     static let reuseIdentifier = String(describing: IssueCell.self)
     
+    // For swipe
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isPagingEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.frame = bounds
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
+    private var visibleView: IssueCellContentView!
+    
     
     // MARK: - Initializer
     
@@ -20,12 +39,14 @@ final class IssueCell: UICollectionViewCell {
         super.init(frame: frame)
         
         setUpView()
+        setUpSwipable()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
         setUpView()
+        setUpSwipable()
     }
 
     // MARK: - Method
@@ -33,6 +54,41 @@ final class IssueCell: UICollectionViewCell {
     override func prepareForReuse() {
         visibleView.initLabels()
     }
+    
+    private func setUpSwipable() {
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
+
+        scrollView.pinEdgesToSuperView()
+        stackView.pinEdgesToSuperView()
+        
+        visibleView = IssueCellContentView()
+        
+        let hiddenView = UIView()
+        hiddenView.backgroundColor = UIColor.blue
+        
+        let hiddenView2 = UIView()
+        hiddenView2.backgroundColor = UIColor.systemRed
+        
+        stackView.addArrangedSubview(visibleView)
+        stackView.addArrangedSubview(hiddenView)
+        stackView.addArrangedSubview(hiddenView2)
+        
+        
+        visibleView.translatesAutoresizingMaskIntoConstraints = false
+        hiddenView.translatesAutoresizingMaskIntoConstraints = false
+        hiddenView2.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 1.4),
+            stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            
+            visibleView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            hiddenView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.2),
+            hiddenView2.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.2)
+        ])
     }
     
     private func setUpView() {
