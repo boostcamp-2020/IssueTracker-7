7801402845e1f6e37f2f30605b4978ebf4bfa22f
namjin <domino8788@naver.com>
2020-11-08 22:17:17 +0900

    (#124) feat: label 수정 라우팅 구현

--- backend/routes/api/label.js
+++ backend/routes/api/label.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const labelController = require('../../controllers/label');
 
-router.get('/', labelController.get);
+router.get('/', labelController.getAll);
 router.post('/', labelController.add);
+router.put('/:label_id', labelController.update);
 
 module.exports = router;
