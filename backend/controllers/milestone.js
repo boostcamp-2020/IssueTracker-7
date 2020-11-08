40ef192034497501b33bb3fb98ad4e251e9cf44a
namjin <domino8788@naver.com>
2020-11-08 21:17:45 +0900

    (#7) feat: milestone 수정 controller 구현

--- backend/controllers/milestone.js
+++ backend/controllers/milestone.js
@@ -9,3 +9,8 @@ exports.add = async (req, res) => {
   const { status, data } = await milestoneService.create(req.body);
   return res.status(status).json(data);
 };
+
+exports.update = async (req, res) => {
+  const { status, data } = await milestoneService.update(req.params, req.body);
+  return res.status(status).json(data);
+};
