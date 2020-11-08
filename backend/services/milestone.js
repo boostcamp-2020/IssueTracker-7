798722b195cdc46837d16171faf0fda244a0a22c
namjin <domino8788@naver.com>
2020-11-08 21:05:32 +0900

    (#7) feat: milestone 생성 service 구현

--- backend/services/milestone.js
+++ backend/services/milestone.js
@@ -1,10 +1,33 @@
 const { Milestone } = require('../models');
 
-exports.getAll = async (req, res) => {
+exports.getAll = async () => {
   try {
-    const result = await Milestone.findAll({ attributes: ['id', 'title', 'due_date'] });
+    const result = await Milestone.findAll({
+      attributes: ['id', 'title', 'due_date', 'description'],
+    });
     return { status: 200, data: result };
   } catch (err) {
     return { status: 401, data: { message: '유효하지 않은 요청 입니다.' } };
+  }
+};
+
+exports.create = async ({ title, due_date, description }) => {
+  try {
+    const milestone = await Milestone.findOrCreate({
+      where: {
+        title,
+      },
+      defaults: {
+        due_date,
+        description,
+      },
+    });
+    if (milestone[1]) {
+      return { status: 200, data: milestone[0] };
+    } else {
+      return { status: 401, data: { message: '제목이 중복되는 milestone이 있습니다.' } };
+    }
+  } catch (err) {
+    return { status: 401, data: { message: '유효하지 않은 입력 입니다.' } };
   }
 };
