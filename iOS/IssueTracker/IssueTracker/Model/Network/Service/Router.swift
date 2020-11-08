0aa037e36636b9a88ca0c5e1bd940115d9015188
tksrl0379 <tksrl0379@naver.com>
2020-11-09 00:43:50 +0900

    (#178) refactor: Router 관련 구조 변경
    What
    1. URLSession 을 사용하는 Router 와 단순히 Json 데이터만 반환하는 Mock Router 를 위해 이 둘의 공통적인 속성을 정의하는 Routable 프로토콜을 만들었습니다
    2. 클래스에서 제네릭을 사용했던 기존 구조는 프로토콜을 통해 묶는게 불가능하기 때문에 Router 클래스의 제네릭을 없앴습니다

--- iOS/IssueTracker/IssueTracker/Model/Network/Service/Router.swift
+++ iOS/IssueTracker/IssueTracker/Model/Network/Service/Router.swift
@@ -7,48 +7,15 @@
 
 import UIKit
 
-
-enum APIError: Error {
-    case data,
-         decodingJSON
-    case redirection,  // 300 번
-         client,        // 400 번
-         server,        // 500 번
-         failed
-}
-
-final class Router<EndPoint: EndPointable> {
+final class Router: Routable {
     private var task: URLSessionTask?
     
-    func openSite(from route: EndPoint) {
+    func openSite(from route: EndPointable) {
         guard let url = URL(string: "\(route.baseURL)\(route.query)") else { return }
         UIApplication.shared.open(url)
     }
     
-    func handleNetworkResponseError(_ response: HTTPURLResponse) -> APIError? {
-        switch response.statusCode {
-        case 200...299: return nil
-        case 300...399: return .redirection
-        case 401...500: return .client
-        case 501...599: return .server
-        default: return .failed
-        }
-    }
-    
-    private func configureRequest(from route: EndPoint) -> URLRequest {
-        let url = route.query == "" ? route.baseURL : route.baseURL.appendingPathComponent(route.query)
-        var request = URLRequest(url: url)
-    
-        request.httpMethod = route.httpMethod?.rawValue
-        request.httpBody = route.bodies?.encode().data(using: String.Encoding.utf8)
-        route.headers?.forEach { key, value in
-            request.setValue(value, forHTTPHeaderField: key)
-        }
-        
-        return request
-    }
-   
-    func request<T: Decodable>(route: EndPoint, completionHandler: ((Result<T,APIError>) -> Void)? = nil) {
+    func request<T: Decodable>(route: EndPointable, completionHandler: ((Result<T,APIError>) -> Void)? = nil) {
         let session = URLSession.shared
         
         let request = configureRequest(from: route)
@@ -92,4 +59,6 @@ extension Dictionary {
         }.joined(separator: "&")
     }
 }
+
+
 
