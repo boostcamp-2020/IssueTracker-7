a6bcd80112a566895f05d11a31e9b6cbdff4da5e
namjin <domino8788@naver.com>
2020-11-08 21:10:12 +0900

    (#7) feat: 전체 milestone에 대한 API 라우팅 구현

--- backend/routes/api/milestone.js
+++ backend/routes/api/milestone.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const milestoneController = require('../../controllers/milestone');
 
 router.get('/', milestoneController.getAll);
+router.post('/', milestoneController.add);
+router.put('/:milestone_id', milestoneController.update);
+router.delete('/:milestone_id', milestoneController.delete);
 
 module.exports = router;
