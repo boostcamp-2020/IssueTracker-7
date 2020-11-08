85488209a491a333147cc5cf05cc39fd0d0990a5
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-08 23:06:17 +0900

    (#171) feat: 레이블 컨트롤러의 response 양식 통일

--- backend/controllers/label.js
+++ backend/controllers/label.js
@@ -1,16 +1,12 @@
 const labelService = require('../services/label');
 
 exports.get = async (req, res) => {
-    const labels = await labelService.getAll(req.params.issue_id);
-    if (labels) {
-        res.json(labels);
-    } else {
-        res.status(401).json('유효하지 않은 요청입니다.');   
-    };
+    const { status, data } = await labelService.getAll(req.params.issue_id);
+    res.status(status).json(data);
 };
 
 exports.add = async (req, res) => {
     const newLabel = req.body;
-    const result = await labelService.create(newLabel);
-    res.json(result);
+    const { status, data } = await labelService.create(newLabel);
+    res.status(status).json(data);
 };
