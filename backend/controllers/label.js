8b7bb3d1920a045d882762601bcde618848d472b
namjin <domino8788@naver.com>
2020-11-08 22:25:01 +0900

    (#126) feat: 레이블을 삭제하는 controller 구현

--- backend/controllers/label.js
+++ backend/controllers/label.js
@@ -1,13 +1,18 @@
 const labelService = require('../services/label');
 
 exports.get = async (req, res) => {
-    const labels = await labelService.getAll();
-    if (labels) res.json(labels);
-    res.status(401).json('유효하지 않은 요청입니다.');
+  const labels = await labelService.getAll();
+  if (labels) res.json(labels);
+  res.status(401).json('유효하지 않은 요청입니다.');
 };
 
 exports.add = async (req, res) => {
-    const newLabel = req.body;
-    const result = await labelService.create(newLabel);
-    res.json(result);
+  const newLabel = req.body;
+  const result = await labelService.create(newLabel);
+  res.json(result);
+};
+
+exports.delete = async (req, res) => {
+  const { status, data } = await labelService.delete(req.params);
+  res.status(status).json(data);
 };
