a18cfc26ce2f2c7697213bb4fc934669ea9224c6
namjin <domino8788@naver.com>
2020-11-08 16:56:08 +0900

    (#7) feat: 전체 milestone 조회 controller 구현

--- backend/controllers/milestone.js
+++ backend/controllers/milestone.js
@@ -1,8 +1,6 @@
-const { Milestone } = require('../models');
+const milestoneService = require('../services/milestone');
 
-exports.get = (req, res) => {
-    Milestone.findAll({
-        attributes: ['id', 'title', 'due_date']
-    }).then(milestones => res.status(200).json(milestones))
-        .catch(err => res.status(401).send('유효하지 않은 요청입니다.'));
-} 
+exports.getAll = async (req, res) => {
+  const { status, data } = await milestoneService.getAll();
+  return res.status(status).json(data);
+};
