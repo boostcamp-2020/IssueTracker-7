4d97c7728afdb2a9c69451501c47aa7c065ea7ec
namjin <domino8788@naver.com>
2020-11-08 22:25:27 +0900

    (#126) feat: 레이블을 삭제하는 service 구현

--- backend/services/label.js
+++ backend/services/label.js
@@ -1,35 +1,51 @@
 const { Label } = require('../models');
 
 exports.getAll = async () => {
-    let result;
-    try {
-        result = await Label.findAll({ attributes: ['id', 'name', 'description', 'color'] });
-    } catch (err) {
-        return false;
-    }
-    return result;
-
-}
+  let result;
+  try {
+    result = await Label.findAll({ attributes: ['id', 'name', 'description', 'color'] });
+  } catch (err) {
+    return false;
+  }
+  return result;
+};
 exports.findByName = async (label) => {
-    let result;
-    try {
-        result = await Label.findOne({ where: { name: label.name } });
-    } catch (err) {
-        return false;
-    }
-    return result;
-}
+  let result;
+  try {
+    result = await Label.findOne({ where: { name: label.name } });
+  } catch (err) {
+    return false;
+  }
+  return result;
+};
 
 exports.create = async (newLabel) => {
-    const { name, description, color } = newLabel;
-    try {
-        const result = await Label.findOrCreate({
-            where: { name: name },
-            defaults: { description: description, color: color }
-        });
-        if (result[1]) return { status: 200, data: result[0] };
-        else return { status: 401, data: { message: '이미 레이블이 존재합니다.' } };
-    } catch (err) {
-        return { status: 401, data: { message: '유효하지 않은 입력입니다.'}};
+  const { name, description, color } = newLabel;
+  try {
+    const result = await Label.findOrCreate({
+      where: { name: name },
+      defaults: { description: description, color: color },
+    });
+    if (result[1]) return { status: 200, data: result[0] };
+    else return { status: 401, data: { message: '이미 레이블이 존재합니다.' } };
+  } catch (err) {
+    return { status: 401, data: { message: '유효하지 않은 입력입니다.' } };
+  }
+};
+
+exports.delete = async ({ label_id }) => {
+  try {
+    const result = await Label.destroy({
+      where: {
+        id: label_id,
+      },
+    });
+    if (result) {
+      return { status: 200, data: { message: '삭제 되었습니다.' } };
+    } else {
+      return { status: 401, data: { message: '유효하지 않은 레이블 입니다.' } };
     }
-}
+  } catch (err) {
+    return { status: 401, data: { message: '유효하지 않은 입력 입니다.' } };
+  }
+};
