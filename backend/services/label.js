181d8520924a35360a84bb39dd58e43650f5773a
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-08 21:06:16 +0900

    (#124) feat: 레이블 리턴 값 수정

--- backend/services/label.js
+++ backend/services/label.js
@@ -20,15 +20,16 @@ exports.findByName = async (label) => {
     return result;
 }
 
-exports.create = async (label) => {
+exports.create = async (newLabel) => {
     const { name, description, color } = newLabel;
     try {
         const result = await Label.findOrCreate({
             where: { name: name },
             defaults: { description: description, color: color }
         });
-        return result[1];
+        if (result[1]) return { status: 200, data: result[0] };
+        else return { status: 401, data: { message: '이미 레이블이 존재합니다.' } };
     } catch (err) {
-        return false;
+        return { status: 401, data: { message: '유효하지 않은 입력입니다.'}};
     }
 }
