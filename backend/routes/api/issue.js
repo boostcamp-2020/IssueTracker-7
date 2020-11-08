a42c12917e5d116a1e2e3443563847f482f41e5a
namjin <domino8788@naver.com>
2020-11-08 16:39:35 +0900

    (#174) feat: 이슈 생성 라우팅 경로 추가

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const issueController = require('../../controllers/issue');
 
 router.get('/', issueController.getAll);
+router.post('/', issueController.add);
 router.get('/:issue_id', issueController.getOne);
 router.put('/:issue_id', issueController.update);
 
