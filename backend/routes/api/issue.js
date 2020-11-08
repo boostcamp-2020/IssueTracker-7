70ce7cfba2d469437fee9269c17454776ded141a
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-09 01:06:13 +0900

    (#171) feat: 레이블 컨트롤러 -  메소드 명 수정 반영

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -12,7 +12,7 @@ router.get('/:issue_id/milestone', issueController.getMilestone);
 router.post('/:issue_id/milestone/:milestone_id', issueController.addMilestone);
 router.delete('/:issue_id/milestone', issueController.deleteMilestone);
 
-router.get('/:issue_id/label', labelController.get);
+router.get('/:issue_id/label', labelController.getAll);
 router.post('/:issue_id/label', issueController.addLabel);
 
 module.exports = router;
