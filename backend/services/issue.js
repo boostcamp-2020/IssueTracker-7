5b904885868d66391373c61bad802f3a0af5d67e
namjin <domino8788@naver.com>
2020-11-08 20:57:03 +0900

    (#176) fix: conflict 해결 중 삭제된 로직 복구

--- backend/services/issue.js
+++ backend/services/issue.js
@@ -191,3 +191,17 @@ exports.update = async ({ issue_id }, { title, status }) => {
     return { status: 401, data: { message: '유효하지 않은 입력 입니다.' } };
   }
 };
+
+exports.create = async ({ title, milestone_id, author_id }) => {
+  try {
+    const result = await Issue.findOrCreate({
+      where: { title },
+      defaults: { milestone_id: milestone_id, user_id: author_id },
+    });
+    if (result[1]) {
+      return { status: 200, data: result[0] };
+    } else return { status: 401, data: { message: '제목이 중복되는 이슈가 있습니다.' } };
+  } catch (err) {
+    return { status: 401, data: { message: '유효하지 않은 입력 입니다.' } };
+  }
+};
