6bdf8bb7f71be23802b24ee8dbdbdcdc2171ffe7
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-09 12:43:04 +0900

    (#171) feat: 레이블 서비스 - 파라미터 여부에 따른 분기처리 사항 반영 취소

--- backend/services/label.js
+++ backend/services/label.js
@@ -1,33 +1,17 @@
-const { Label, label_has_issue } = require('../models');
+const { Label } = require('../models');
 
-exports.getAll = async (issue) => {
-    let result;
-    try {
-        if (issue) {
-            result = await Label.findAll({
-                attributes: ['id', 'name', 'description', 'color'],
-                include: [
-                    {
-                        model: label_has_issue,
-                        where: {
-                            issue_id: issue
-                        },
-                        attributes: ['label_id']
-                    }
-                ]
-            });
-        } else {
-            result = await Label.findAll({
-                attributes: ['id', 'name', 'description', 'color']
-            });
-        };
-    } catch (err) {
-        return { status: 401, data: { message: '유효하지 않은 입력입니다.' } };
-    };
-    if (result) return { status: 200, data: result };
-    else return { status: 401, data: { message: '유효하지 않은 입력입니다.' } };
+exports.getAll = async () => {
+  let result;
+  try {
+      result = await Label.findAll({
+          attributes: ['id', 'name', 'description', 'color']
+      });
+  } catch (err) {
+      return { status: 401, data: { message: '유효하지 않은 입력입니다.' } };
+  };
+  if (result) return { status: 200, data: result };
+  else return { status: 401, data: { message: '유효하지 않은 입력입니다.' } };
 };
-
 
 exports.getOne = async ({ label_id }) => {
   try {
