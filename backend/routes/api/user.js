1f60c2b71a2c94d961f76192d7896a1a14238697
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-09 16:27:03 +0900

    (#172) feat: 유저 컨트롤러의 함수명 수정

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/user');
 
-router.get('/', userController.get);
+router.get('/', userController.getAll);
 
 module.exports = router; 
