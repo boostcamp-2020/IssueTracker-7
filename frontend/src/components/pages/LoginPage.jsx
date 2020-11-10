d6ca9c1a4ff9f264f99f0d2cb15bea7bbc4170b3
namjin <domino8788@naver.com>
2020-11-11 00:13:12 +0900

    (#14) feat: LoginPage 구현

---
+++ frontend/src/components/pages/LoginPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import LoginPageTemplate from '@templates/LoginPageTemplate';
+import '@styles/LoginPage.css';
+
+const LoginPage = () => {
+  return <LoginPageTemplate />;
+};
+
+export default LoginPage;
