0d58e25ea335373593195c83b595e79a055c4eb6
namjin <domino8788@naver.com>
2020-11-10 23:49:44 +0900

    (#14) feat: Label atom 컴포넌트 구현

---
+++ frontend/src/components/atoms/Label.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.p`
+  margin-bottom: 5px;
+  font-weight: bold;
+  font-size: 15px;
+`;
+
+export default Label;
