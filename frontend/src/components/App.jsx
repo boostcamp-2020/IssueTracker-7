c7c219fb84167390371f4af818b150c204e8d0f5
namjin <domino8788@naver.com>
2020-11-12 12:56:24 +0900

    (#14) feat: 인증 로직을 App.js 로 이식 및 Provider 적용

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import LoginPage from '@pages/LoginPage';
 import IssuePage from '@pages/IssuePage';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { request } from '@utils/request';
+import { IssueProvider } from '@stores/issue';
+import { AuthProvider, AuthContext } from '@stores/auth';
 
+const isAuthenticated = async () => await request('GET', '/api/auth/token');
 const App = () => {
+  const storedAccessToken = localStorage.getItem('accessToken');
+  useEffect(async () => {
+    if (location.pathname != '/login' && !storedAccessToken) {
+      const { status, data } = await isAuthenticated();
+      if (data.accessToken) {
+        localStorage.setItem('accessToken', data.accessToken);
+      } else {
+        location.href = '/login';
+      }
+    }
+  }, []);
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/issue" component={IssuePage} />
-        <Redirect path="*" to="/issue" />
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <IssueProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/issue" component={IssuePage} />
+            <Redirect path="*" to="/issue" />
+          </Switch>
+        </BrowserRouter>
+      </IssueProvider>
+    </AuthProvider>
   );
 };
 
