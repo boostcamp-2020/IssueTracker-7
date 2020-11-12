26d5cc5fb67239a1ce87d0ad6686f1bafd0beae0
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-12 16:44:19 +0900

    (#162) feat: index를 이용한 molecules 컴포넌트 접근법 적용

---
+++ frontend/src/components/atoms/Wrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    min-width: 1000px;
+    width: 100%
+    height: 100%;
+    margin: 0;
+`;
+
+export default Wrapper;
