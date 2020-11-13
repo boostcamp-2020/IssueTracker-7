f72cbe0f47ea47f35d0642a56a1e194b02eafeca
namjin <domino8788@naver.com>
2020-11-13 11:40:02 +0900

    (#12) feat: Icon 컴포넌트 적용

--- frontend/src/components/molecules/GithubButton.jsx
+++ frontend/src/components/molecules/GithubButton.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import A from '@atoms/';
 
 const GithubButton = ({ onClick }) => {
   return (
-    <A.Button onClick={onClick} backgroundColor="#a0a0a0" color='#ffffff'>
-      Sign with GitHub <FontAwesomeIcon icon={faGithub} color="black" size="lg" />
+    <A.Button onClick={onClick} backgroundColor="#a0a0a0" color="#ffffff">
+      Sign with GitHub <A.Icon icon={faGithub} color="black" size="lg" />
     </A.Button>
   );
 };
