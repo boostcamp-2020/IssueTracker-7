4935eccfc0b5fbb3e964b389f5da97be8e7abb57
namjin <domino8788@naver.com>
2020-11-13 11:37:38 +0900

    (#22) feat: store와 추가 필터 표시 조건 바인딩

--- frontend/src/components/molecules/DropdownWithCheck.jsx
+++ frontend/src/components/molecules/DropdownWithCheck.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import A from '@atoms/';
+import { useFilter } from '@stores/filter';
 
 const List = styled.div`
-    display: flex;
-    height: 35px;
-    margin: 0;
-    background-color: ${(props) => props.backgroundColor};
-    border: none;
-    &:hover {
-        background-color: #EAECEF;
-        cursor: pointer;
-    }
+  display: flex;
+  height: 35px;
+  margin: 0;
+  background-color: ${(props) => props.backgroundColor};
+  border: none;
+  &:hover {
+    background-color: #eaecef;
+    cursor: pointer;
+  }
 `;
 
-const DropdownWithCheck = ({ children }) => {
-    const [check, setCheck] = useState(false)
-    
-    return (
-        <>
-            { check ?
-                <List onClick={() => { setCheck(false) }} backgroundColor='#EAECEF'><A.Check />{children}</List>
-                : <List onClick={() => { setCheck(true) }}><A.Uncheck />{children}</List>
-            }
-        </>
-    );
+const DropdownWithCheck = ({ type, value, onClick, children }) => {
+  const { filters } = useFilter();
+  return (
+    <>
+      {filters[type] == value && type != 'is' && value != '@me' ? (
+        <List onClick={(event) => onClick(type, value)()} backgroundColor="#EAECEF">
+          <A.Check />
+          {children}
+        </List>
+      ) : (
+        <List onClick={(event) => onClick(type, value)()}>
+          <A.Uncheck />
+          {children}
+        </List>
+      )}
+    </>
+  );
 };
-
 
 export default DropdownWithCheck;
