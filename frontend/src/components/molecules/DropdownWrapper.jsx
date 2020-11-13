525eed55d49b87ad45ff6529caca0e96ecf8768a
namjin <domino8788@naver.com>
2020-11-13 11:38:25 +0900

    (#22) style: prettier 적용

--- frontend/src/components/molecules/DropdownWrapper.jsx
+++ frontend/src/components/molecules/DropdownWrapper.jsx
@@ -4,28 +4,27 @@ import A from '@atoms/';
 import M from '@molecules/';
 
 const Wrapper = styled.div`
-    position: absolute;
-    height: fit-content;
-    right: ${(props) => props.right};
-    width: 300px;
-    margin-top: 60px;
-    border: 1px solid #cccccc;
-    border-radius: 5px;
-    background-color: #ffffff;
-    z-index: 100;
+  position: absolute;
+  height: fit-content;
+  right: ${(props) => props.right};
+  width: 300px;
+  margin-top: 60px;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+  background-color: #ffffff;
+  z-index: 100;
 `;
 
 const DropdownWrapper = ({ name, onClick, right, children }) => {
-    
-    return (
-        <>
-            <A.DropdownOverlay onClick={onClick} />
-            <Wrapper right={right}>
-                <M.DropdownHeader onClick={onClick}>{children}</M.DropdownHeader>
-                <M.Dropdown name={name}/>
-            </Wrapper>
-        </>
-    );
+  return (
+    <>
+      <A.DropdownOverlay onClick={onClick} />
+      <Wrapper right={right}>
+        <M.DropdownHeader onClick={onClick}>{children}</M.DropdownHeader>
+        <M.Dropdown name={name} />
+      </Wrapper>
+    </>
+  );
 };
 
 export default DropdownWrapper;
