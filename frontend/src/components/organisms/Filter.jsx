a1b0e47b12e97821e00550d06a468bba02b816cf
namjin <domino8788@naver.com>
2020-11-13 11:29:48 +0900

    (#24 #25 #27 #28) feat: 필터 적용에 필요한 데이터 요청 구현

--- frontend/src/components/organisms/Filter.jsx
+++ frontend/src/components/organisms/Filter.jsx
@@ -1,62 +1,92 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLabel } from '@stores/label';
+import { request } from '@utils/request';
+import { useMilestone } from '@stores/milestone';
+import { useUser } from '@stores/user';
 import styled from 'styled-components';
-import A from '@atoms/'
+import A from '@atoms/';
 import M from '@molecules/';
 
 const FilterBox = styled.div`
-    height: 60px;
-    margin: 30px auto 0 auto;
-    border: 1px solid #d6d8db;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    background-color: #EAECEF;
-    display:flex;
-    flex-direction:${(props) => props.direction || "row"};
-    justify-content: flex-end;
+  height: 60px;
+  margin: 30px auto 0 auto;
+  border: 1px solid #d6d8db;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  background-color: #eaecef;
+  display: flex;
+  flex-direction: ${(props) => props.direction || 'row'};
+  justify-content: flex-end;
 
-    ${A.FlexBox}{
-        padding-top: 20px;
-        padding-left: 12px;
-    }
-    ${A.FlexItem} {
-        justify-content: flex-end;
-    }
+  ${A.FlexBox} {
+    padding-top: 20px;
+    padding-left: 12px;
+  }
+  ${A.FlexItem} {
+    justify-content: flex-end;
+  }
 `;
 
 const filters = [
-    {
-        name: 'Author',
-        title: 'Filter by author',
-        right: '300px'
-    }, {
-        name: 'Label',
-        title: 'Filter by label',
-        right: '200px'
-    }, {
-        name: 'Milestones',
-        title: 'Filter by milestone',
-        right: '100px'
-    }, {
-        name: 'Assignee',
-        title: 'Filter by assignee',
-        right: '50px'
-    }
+  {
+    name: 'Author',
+    title: 'Filter by author',
+    right: '300px',
+  },
+  {
+    name: 'Label',
+    title: 'Filter by label',
+    right: '200px',
+  },
+  {
+    name: 'Milestones',
+    title: 'Filter by milestone',
+    right: '100px',
+  },
+  {
+    name: 'Assignee',
+    title: 'Filter by assignee',
+    right: '50px',
+  },
 ];
 
 const Filter = () => {
-    const contents = filters.map(filter => {
-        return <M.DropdownButton name={filter.name} key={filter.name} right={filter.right}>{filter.title}</M.DropdownButton>
-    })
+  const { labelDispatch } = useLabel();
+  const { userDispatch } = useUser();
+  const { milestoneDispatch } = useMilestone();
+
+  useEffect(async () => {
+    const labelResponse = await request('GET', `/api/label`);
+    labelDispatch({
+      type: 'GET',
+      data: labelResponse.data,
+    });
+    const userResponse = await request('GET', '/api/user');
+    userDispatch({
+      type: 'GET',
+      data: userResponse.data,
+    });
+    const milestoneResponse = await request('GET', '/api/milestone');
+    milestoneDispatch({
+      type: 'GET',
+      data: milestoneResponse.data,
+    });
+  }, []);
+  const contents = filters.map((filter) => {
     return (
-        <FilterBox direction="row">
-            <A.FlexBox>
-                <A.CheckBox type="checkbox" />
-            </A.FlexBox>
-            <A.FlexItem>
-                {contents}
-            </A.FlexItem>
-        </FilterBox>
+      <M.DropdownButton name={filter.name} key={filter.name} right={filter.right}>
+        {filter.title}
+      </M.DropdownButton>
     );
+  });
+  return (
+    <FilterBox direction="row">
+      <A.FlexBox>
+        <A.CheckBox type="checkbox" />
+      </A.FlexBox>
+      <A.FlexItem>{contents}</A.FlexItem>
+    </FilterBox>
+  );
 };
 
 export default Filter;
